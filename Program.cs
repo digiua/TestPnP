6e086a2ee1d690ad1ace6f2d1ac7504817361dc4
digiua <digiua@abacco.com>
2022-04-14 12:31:43 -0400

    Update Program.cs
    Se ajustan a los comandos del nuevo pnpdll64.dll

--- Program.cs
+++ Program.cs
@@ -1,28 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
+// Proceso basico de generar factura en fiscales pnp con C#
+// https://desarrollospnp.com/pregunta
 using System;
 using System.Runtime.InteropServices;
 
+namespace testcs
+{
+    class Program
+    {
+    [DllImport("pnpdll64.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)]
+    public static extern IntPtr PFultimo();
+    [DllImport("pnpdll64.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)]
+    public static extern IntPtr  PFabrepuerto(string puerto);
+    [DllImport("pnpdll64.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)]
+    public static extern IntPtr  PFabrefiscal(string razon,string rif);
+    [DllImport("pnpdll64.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)]
+    public static extern IntPtr  PFrenglon(string Descripcion,string cantidad,string monto,string iva);
+    [DllImport("pnpdll64.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)]
+    public static extern IntPtr  PFComando(string Comando);
+    [DllImport("pnpdll64.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)]
+    public static extern IntPtr  PFtotal();
+    [DllImport("pnpdll64.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)]
+    public static extern IntPtr  PFTfiscal(string Texto);
+    [DllImport("pnpdll64.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)]
+    public static extern IntPtr  PFultimo(string Texto);
+        static void Main(string[] args)
+        {    Console.WriteLine("HOLA");
+             IntPtr ptr = PFabrepuerto("13");
+             string result =  Marshal.PtrToStringAnsi(ptr);
+             if (result.Equals("ER")) {
+                 Console.WriteLine("ERROR Abriendo Puerto");}
+             Console.WriteLine("PFabrepuerto: .{0}.", result);
+             ptr = PFabrefiscal("NOMBRE EMPRESA","RIFEMP");
+             result =  Marshal.PtrToStringAnsi(ptr);
+             Console.WriteLine("PFabrefiscal: .{0}.", result);
+             if (result.Equals("ER")) {
+                 ptr = PFultimo();
+                 result =  Marshal.PtrToStringAnsi(ptr);
+                 Console.WriteLine("ERROR Abriendo Facrtura: .{0}.", result);}
 
-namespace  FiscalPrinterPnP {
-    public class PnPLoader {
-        [DllImport("pnpdll")] public static extern string PFabrepuerto(String port);
-        [DllImport("pnpdll")] public static extern string PFrepx();
-        [DllImport("pnpdll")] public static extern string PFcierrapuerto();
-    }
-    public class Program {
-        PnPLoader loader;
+             ptr = PFrenglon("Productos","1000","1000","1600");
+             result =  Marshal.PtrToStringAnsi(ptr);
+             Console.WriteLine("PFrenglon: .{0}.", result);
+             if (result.Equals("ER")) {
+                 ptr = PFultimo();
+                 result =  Marshal.PtrToStringAnsi(ptr);
+                 Console.WriteLine("ERROR Renglon producto: .{0}.", result);}
 
-        public Program(string port) {
-            loader = new PnPLoader();
-            string result = PnPLoader.PFabrepuerto(port);    
-            Console.WriteLine("PFabrepuerto: {0}", result);
-            result = PnPLoader.PFrepx();
-            Console.WriteLine("PFrepx: {0}", result);
-            result = PnPLoader.PFcierrapuerto();
-            Console.WriteLine("PFcierrapuerto: {0}", result);
-        }
-        public static void Main(string[] args) {
-            new Program(args[0]);
+             ptr = PFComando("E|B|1000");
+             result =  Marshal.PtrToStringAnsi(ptr);
+             Console.WriteLine("PFComando: .{0}.", result);
+             if (result.Equals("ER")) {
+                 ptr = PFultimo();
+                 result =  Marshal.PtrToStringAnsi(ptr);
+                 Console.WriteLine("ERROR Comando: .{0}.", result);}
+
+             ptr = PFTfiscal("Texto Libre Forma Pago");
+             result =  Marshal.PtrToStringAnsi(ptr);
+             Console.WriteLine("PFTfiscal: .{0}.", result);
+             if (result.Equals("ER")) {
+                 ptr = PFultimo();
+                 result =  Marshal.PtrToStringAnsi(ptr);
+                 Console.WriteLine("ERROR Texto Fiscal: .{0}.", result);}
+             ptr = PFtotal();
+             result =  Marshal.PtrToStringAnsi(ptr);
+             Console.WriteLine("PFtotal: .{0}.", result);
+             if (result.Equals("ER")) {
+                 ptr = PFultimo();
+                 result =  Marshal.PtrToStringAnsi(ptr);
+                 Console.WriteLine("ERROR Cerrando Facrtura: .{0}.", result);}
+
         }
     }
 }
